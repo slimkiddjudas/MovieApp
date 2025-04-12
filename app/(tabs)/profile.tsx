e244f488ea1f60f7ea2e076bee88381ddec19253
umut <umutcerk@gmail.com>
2025-04-12 23:22:40 +0300

    Initial commit

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,17 @@
+import {View, Text, Image} from 'react-native';
+import React from 'react';
+import {icons} from "@/constants/icons";
+
+const Profile = () => {
+    return (
+        <View className="bg-primary flex-1 px-10">
+            <View className="flex justify-center items-center flex-1 flex-col gap-5">
+                <Image source={icons.person} className="size-10" tintColor="#fff" />
+                <Text className="text-white font-bold text-xl">Profile Page</Text>
+                <Text className="text-gray-500 font-semibold text-base">Coming soon!</Text>
+            </View>
+        </View>
+    )
+}
+
+export default Profile;
